739b43bb62563db756e3cfbd0babfeb077f9b937
turim0 <ivan_medvedev_1996@mail.ru>
2022-01-08 15:48:43 +0300

    move queries into QuerySet methods

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,20 @@ from django.db.models import Count
 class PostQuerySet(models.QuerySet):
 
     def year(self, year):
-        posts_at_year = self.filter(published_at__year=year).order_by('published_at')
-        return posts_at_year
+        return self.filter(published_at__year=year).order_by('published_at')
+
+    def popular(self):
+        return self.annotate(likes_count=Count('likes')).order_by('-likes_count')
+
+    def fetch_with_comments_count(self):
+        posts_ids = [post.id for post in self]
+        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(
+            comments_count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+        for post in self:
+            post.comments_count = count_for_id[post.id]
+        return self
 
 
 class TagQuerySet(models.QuerySet):
