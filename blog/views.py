211045c64f05f73f60c0c962a20074ea318de9fc
turim0 <ivan_medvedev_1996@mail.ru>
2022-01-26 21:53:27 +0300

    cleanup the code

--- blog/views.py
+++ blog/views.py
@@ -74,8 +74,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
-    comments = Comment.objects.filter(post=post)
+    post = Post.objects.select_related('author').annotate(Count('likes')).get(slug=slug)
+    comments = Comment.objects.select_related('author').filter(post=post)
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -84,8 +84,6 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
-
     related_tags = post.tags.popular().annotate(Count('posts'))
 
     serialized_post = {
@@ -93,7 +91,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.likes__count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
